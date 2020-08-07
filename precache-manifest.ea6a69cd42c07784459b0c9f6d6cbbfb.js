9491d3538a4c52c1e9c9174b92f38d77c7479cf8
Michael Roy <someramichael31@gmail.com>
2020-08-07 21:51:35 +0800

    Updates

---
+++ precache-manifest.ea6a69cd42c07784459b0c9f6d6cbbfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7e756847302e0c0063ffc0107fb6caf",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "72a889d8a1c2970893c7",
+    "url": "/monster-rolodex/static/css/main.c77f342b.chunk.css"
+  },
+  {
+    "revision": "5ceb71a0b3552c2613c8",
+    "url": "/monster-rolodex/static/js/2.95da3331.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.95da3331.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72a889d8a1c2970893c7",
+    "url": "/monster-rolodex/static/js/main.935033e0.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
